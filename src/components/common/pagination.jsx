db0c7bcf74979b6dc17102830f17c2309264a19f
Patrick Bald <pbald@nd.edu>
2020-01-12 11:03:17 -0500

    Added Pagination

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import _ from "lodash";
+import PropTypes from "prop-types";
 
 const Pagination = props => {
-  // destructuring
+  // destructuring props
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
 
   const pagesCount = Math.ceil(itemsCount / pageSize);
@@ -26,6 +27,13 @@ const Pagination = props => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default Pagination;
